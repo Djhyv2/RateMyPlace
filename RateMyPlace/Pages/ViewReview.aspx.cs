d446b0fdc68b3023291b04349cad40b4a5d8a91a
Connor <connofitz@gmail.com>
2018-10-13 13:02:07 -0500

    Added dropdown menu functionality to all pages

--- RateMyPlace/Pages/ViewReview.aspx.cs
+++ RateMyPlace/Pages/ViewReview.aspx.cs
@@ -11,17 +11,38 @@ namespace RateMyPlace.Pages
 {
     public partial class SearchResults : System.Web.UI.Page
     {
+        private static DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            /*DataTable table = new DataTable();
-            table = Connection.RunSQL( "SELECT * FROM Reviews" );
-
-            foreach ( DataRow row in table.Rows )
-            {
-
-            }*/
+            
         }
 
+        public static string PopulateReviews()
+        {
+            dt = Connection.RunSQL("SELECT * FROM Reviews");
+
+            string html = "<table>";
+            //add header row
+            html += "<tr>";
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                html += "<td>" + dt.Columns[i].ColumnName + "</td>";
+            }
+            html += "</tr>";
+            //add rows
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                html += "<tr>";
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    html += "<td>" + dt.Rows[i][j].ToString() + "</td>";
+                }
+                html += "</tr>";
+            }
+            html += "</table>";
+            return html;
+        }
 
     }
 }
