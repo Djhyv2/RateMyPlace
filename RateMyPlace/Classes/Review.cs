3b393f3bb7fde3196e2cb233dcd7808c7ae78001
Connor <connofitz@gmail.com>
2018-10-12 22:12:12 -0500

    Backend for the review popup

---
+++ RateMyPlace/Classes/Review.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RateMyPlace.Classes
+{
+    public class Review
+    {
+        private int rating;
+
+        public Review()
+        {
+
+        }
+
+        public void SendSQLCommand( string command )
+        {
+            //Send the sqlCommand variable to the database here.
+        }
+
+        public int Rating
+        {
+            get
+            {
+                return rating;
+            }
+            set
+            {
+                rating = value;
+            }
+        }
+    }
+}
---
+++ RateMyPlace/Pages/ReviewPopup.aspx.cs
@@ -0,0 +1,55 @@
+﻿using RateMyPlace.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RateMyPlace.Pages
+{
+    public partial class ReviewPopup : System.Web.UI.Page
+    {
+        Review review;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            review = new Review();
+        }
+
+        private void OneStarClick( object sender, EventArgs e)
+        {
+            review.Rating = 1;
+        }
+
+        private void TwoStarClick( object sender, EventArgs e)
+        {
+            review.Rating = 2;
+        }
+
+        private void ThreeStarClick(object sender, EventArgs e)
+        {
+            review.Rating = 3;
+        }
+
+        private void FourStarClick(object sender, EventArgs e)
+        {
+            review.Rating = 4;
+        }
+
+        private void FiveStarClick( object sender, EventArgs e)
+        {
+            review.Rating = 5;
+        }
+
+        private void AcceptClick(object sender, EventArgs e) //When the user accepts their rating as final
+        {
+            review.SendSQLCommand( "INSERT INTO REVIEWS VALUES (" ); //Need the review table schema for the rest of this. 
+        }
+
+        private void CancelClick( object sender, EventArgs e)
+        {
+            //Close the window here
+        }
+    }
+}
