265acc60d3c327d687dafb48e54805ade4c71dc2
djhyv2 <djhyv2@mail.missouri.edu>
2018-10-13 11:18:50 -0500

    Hotfix to comment out bugs

--- RateMyPlace/Pages/Login.aspx.cs
+++ RateMyPlace/Pages/Login.aspx.cs
@@ -8,14 +8,14 @@ namespace RateMyPlace.Pages
 {
     public partial class Login : System.Web.UI.Page
     {
-        protected void handleLogin_click(object sender, EventArgs e)
-        {
-            List<SqlParameter> parameters = new List<SqlParameter>;
-            parameters.AddWithValue("@Username", txtUsername.Text);
-            DataTable credentials = Connection.RunSQL("SELECT PK_Username, Password FROM Users WHERE PK_Username = '" + txtUsername.Text + "'",parameters);
+        //protected void handleLogin_click(object sender, EventArgs e)
+        //{
+        //    List<SqlParameter> parameters = new List<SqlParameter>;
+        //    parameters.AddWithValue("@Username", txtUsername.Text);
+        //    DataTable credentials = Connection.RunSQL("SELECT PK_Username, Password FROM Users WHERE PK_Username = '" + txtUsername.Text + "'",parameters);
 
-            MessageBox.Show("Server Username: " + credentials.Rows[0]["PK_Username"] + "\nServer Password: " + credentials.Rows[0]["Password"] + "\nInput Username: " + txtUsername.Text + "\nInput Password: " + txtPassword.Text, "Confidential Info", MessageBoxButtons.OK);
-        }
+        //    MessageBox.Show("Server Username: " + credentials.Rows[0]["PK_Username"] + "\nServer Password: " + credentials.Rows[0]["Password"] + "\nInput Username: " + txtUsername.Text + "\nInput Password: " + txtPassword.Text, "Confidential Info", MessageBoxButtons.OK);
+        //}
 
         protected void handleRegister_click(object sender, EventArgs e)
         {
