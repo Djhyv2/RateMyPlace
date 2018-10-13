944ef80bd02e25415769c866ebdb09f4c7003839
Connor <connofitz@gmail.com>
2018-10-13 18:41:36 -0500

    Nicer review UI

--- RateMyPlace/Pages/ViewReview.aspx.cs
+++ RateMyPlace/Pages/ViewReview.aspx.cs
@@ -11,7 +11,17 @@ namespace RateMyPlace.Pages
 {
     public partial class SearchResults : System.Web.UI.Page
     {
+        //Needs complex, overall rating, average rent cost, pet, furnished, parking, study spaces, shuttle, gym
         private static DataTable table;
+        private string complex;
+        private float rating = 0;
+        private float rent = 0;
+        private string pet = "";
+        private string furnished = "";
+        private string parking = "";
+        private string studySpaces = "";
+        private string shuttle = "";
+        private string gym = "";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -22,31 +32,94 @@ namespace RateMyPlace.Pages
         {
             table = Connection.RunSQL("SELECT * FROM Reviews");
 
-            string html = "<div style= \"margin-top: 90px;\"><table>";
-            html += "<th>Username</th><th>Overall Rating</th><th>Noise Rating</th><th>Safety Rating</th>" +
-                "<th>Maintenance</th><th>Lease Start Date</th><th>Lease End Date</th><th>Distance from Campus</th>" +
-                "<th>Pros</th><th>Cons</th><th>Study Space</th><th>Shuttle</th><th>Wifi</th>" +
-                "<th>Wifi</th><th>Furnished</th><th>Cable</th><th>Trash Services</th>" +
-                "<th>Gym</th><th>Parking</th><th>Parking Fee</th><th>Pets</th><th>Pets Fee</th>" +
-                "<th>Misc Fees</th><th>Rent</th><th>Utilities</th><th>Housing Complex</th>";
+            string html = "<div style= \"margin-top: 85px;\">";
 
-                for ( int row = 0; row < table.Rows.Count; row++)
+            for ( int row = 0; row < table.Rows.Count; row++) //For each review
+            {
+                for( int col = 0; col < table.Columns.Count; col++)
                 {
-                    html += "<tr>";
-
-                    for( int col = 1; col < table.Columns.Count; col++)
+                    //Needs complex, overall rating, average rent cost, pet, furnished, parking, study spaces, shuttle, gym
+                    switch ( col )
                     {
-                            html += "<td>";
-
-                            html += table.Rows[row][col].ToString();
-
-                            html += "</td>";
+                        case 25:
+                            complex = table.Rows[row][col].ToString();
+                            break;
+                        case 11:
+                            if (table.Rows[row][col].ToString() == "True")
+                                studySpaces = "X";
+                            break;
+                        case 12:
+                            if (table.Rows[row][col].ToString() == "True")
+                                shuttle = "X";
+                            break;
+                        case 14:
+                            if (table.Rows[row][col].ToString() == "True")
+                                furnished = "X";
+                            break;
+                        case 20:
+                            if (table.Rows[row][col].ToString() == "True")
+                                pet = "X";
+                            break;
+                        case 18:
+                            if (table.Rows[row][col].ToString() == "True")
+                                parking = "X";
+                            break;
+                        case 17:
+                            if (table.Rows[row][col].ToString() == "True")
+                                gym = "X";
+                            break;
                     }
 
-                    html += "</tr>";
                 }
 
-            html += "</table></div>";
+                if (complex != null && rent == 0)
+                {
+                    DataTable temp = Connection.RunSQL("SELECT Rent FROM Reviews WHERE HousingComplex = '" + complex + "';");
+                    float tmp = 0;
+
+                    for (int entries = 0; entries < temp.Rows.Count; entries++)
+                    {
+                        float.TryParse(temp.Rows[entries][0].ToString(), out tmp);
+
+                        rent += tmp;
+
+                        if (entries == temp.Rows.Count - 1)
+                            rent = rent / temp.Rows.Count;
+                    }
+
+                    temp = null;
+                }
+
+                if (complex != null && rating == 0)
+                {
+                    DataTable temp = Connection.RunSQL("SELECT OverallRating FROM Reviews WHERE HousingComplex = '" + complex + "';");
+                    float tmp = 0;
+
+                    for (int entries = 0; entries < temp.Rows.Count; entries++)
+                    {
+                        float.TryParse(temp.Rows[entries][0].ToString(), out rating);
+
+                        rent += tmp;
+
+                        if (entries == temp.Rows.Count - 1)
+                            rent = rent / temp.Rows.Count;
+                    }
+                    temp = null;
+                }
+
+                //Needs complex, overall rating, average rent cost, pet, furnished, parking, study spaces, shuttle, gym
+                html += "<table class=\"ReviewStyle.css\">" +
+                    "<th>Complex Name</th><th>Overall Rating</th><th>Average Rent</th><th>Pet Friendly</th><th>Pre-Furnished</th><th>Available Parking</th><th>Study Spaces</th><th>Free Shuttle</th><th>Free Gym</th>" +
+                    "<tr><td style=\"min-width:200px\">" + complex + "</td>" + "<td>" + rating.ToString() + "</td>" + "<td>" + rent.ToString() + "</td>" + "<td>" + pet + "</td>" + "<td>" + furnished + "</td>" +
+                    "<td>" + parking + "</td>" + "<td>" + studySpaces + "</td>" + "<td>" + shuttle + "</td>" + "<td>" + gym + "</td></tr>" +
+                    "</table>";
+
+                complex = null;
+                rent = 0;
+                rating = 0;
+            }
+
+            html += "</div>";
 
             LiteralText.Text = html;
         }
