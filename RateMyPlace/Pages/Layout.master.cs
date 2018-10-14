cf7d29181f1ca1d9d83ce20bd3311ecb5026de76
djhyv2 <djhyv2@mail.missouri.edu>
2018-10-13 21:21:46 -0500

    Work on master page, adding username. Saving at this point before breaking everything.

---
+++ RateMyPlace/Pages/Layout.master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RateMyPlace.Pages
+{
+    public partial class Layout : System.Web.UI.MasterPage
+    { 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] != null)
+            {
+                lblUsername.Text = "Hello, " + Session["Username"].ToString();
+            }
+        }
+    }
+}
