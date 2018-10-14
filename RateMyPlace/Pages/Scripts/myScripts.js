2c1669b478788ddd2b9ab686772e321709a0ba4f
Joshua Thompson <jthompson706.jt@gmail.com>
2018-10-13 21:48:35 -0500

    Update: homepage carousel reactive

--- RateMyPlace/Pages/Scripts/myScripts.js
+++ RateMyPlace/Pages/Scripts/myScripts.js
@@ -17,6 +17,56 @@
         let complex = tmpString.split("\n");
         $("#compareReviewBody h5.modal-title").text(complex[0]);
     });
+
+    $("img#homePageCarouselSubImage1").css({"transform": "scale(1.1)"});
+
+    function goToSlide(number) {
+        $("#carousel").carousel(number);
+    }
+
+    $("img#homePageCarouselSubImage1").click(function(){
+        $("#homePageCarousel").carousel(0);
+    });
+    $("img#homePageCarouselSubImage2").click(function(){
+        $("#homePageCarousel").carousel(1);
+    });
+    $("img#homePageCarouselSubImage3").click(function(){
+        $("#homePageCarousel").carousel(2);
+    });
+
+    
+    $('#homePageCarousel').bind('slide.bs.carousel', function (e) {
+        setTimeout(function(){ 
+            $( ".carousel-item" ).each(function( index ) {
+                    if($(this).hasClass("active")){
+                        if($(this).is("#item1")){
+                            $("img#homePageCarouselSubImage1").css({"transform": "scale(1.1)"});
+                        }
+                        else if($(this).is("#item2")){
+                            $("img#homePageCarouselSubImage2").css({"transform": "scale(1.1)"});
+                        }
+                        else if($(this).is("#item3")){
+                            $("img#homePageCarouselSubImage3").css({"transform": "scale(1.1)"});
+                        }
+                        console.dir($(this));
+                    }
+                    else {
+                        if($(this).is("#item1")){
+                            $("img#homePageCarouselSubImage1").css({"transform": "scale(1.0)"});
+                        }
+                        else if($(this).is("#item2")){
+                            $("img#homePageCarouselSubImage2").css({"transform": "scale(1.0)"});
+                        }
+                        else if($(this).is("#item3")){
+                            $("img#homePageCarouselSubImage3").css({"transform": "scale(1.0)"});
+                        }
+                    }
+            });
+        }, 1000);
+        
+    });
+
+
    
 });
 
